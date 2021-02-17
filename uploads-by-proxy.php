410a30cf2d6f38d3fc2871b9241b852d39c8b82a
Toni Viemerö <toni.viemero@iki.fi>
2021-02-17 11:46:28 +0200

    Add actions/filters if we're not in production

--- uploads-by-proxy.php
+++ uploads-by-proxy.php
@@ -9,35 +9,13 @@ Version: 1.1.3
 */
 
 /**
- * Check that we're on a development server.
- * This tests if we're serving from and to localhost (127.0.0.1),
- * which should catch most common dev environments like MAMP, WAMP, XAMPP, etc.
- *
- * If you're hosting from a staging environment, or some weird situation where
- * this test doesn't return true, redefine it in wp-config.php:
- *     define('UBP_IS_LOCAL', true);
- *
- * 	   WARNING!!
- *     Do not set this to "true" on a live site!
- *     Doing so will cause 404 pages for wp-content/uploads to go into
- *     an infinite loop until Apache kills the PHP process.
- */
-if ( !defined('UBP_IS_LOCAL') ) {
-	define('UBP_IS_LOCAL', (
-		( '127.0.0.1' == $_SERVER['SERVER_ADDR'] && '127.0.0.1' == $_SERVER['REMOTE_ADDR'] ) // IPv4
-		|| ( '::1' == $_SERVER['SERVER_ADDR'] && '::1' == $_SERVER['REMOTE_ADDR'] ) // IPv6
-	) );
-}
-
-/**
- * Used for deactivating the plugin here or in class-helpers.php if requirements aren't met.
+ * Used for deactivating the plugin here or in class-ubp-helpers.php if requirements aren't met.
  */
 define( 'UBP_PLUGIN_FILE', __FILE__ );
 
 require_once __DIR__ . '/class-ubp-helpers.php';
 
-// Only initialize if we're on a development server
-if ( UBP_IS_LOCAL ) {
+if ( 'production' !== wp_get_environment_type() ) {
 	add_action( 'admin_init', 'UBP_Helpers::requirements_check' );
 	add_filter( '404_template', 'UBP_Helpers::init_404_template' );
 }
